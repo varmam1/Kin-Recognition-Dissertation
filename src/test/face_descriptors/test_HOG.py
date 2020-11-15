307f79c43adfc223b96f7d729b1320797c9c5684
Manu Varma <mv465@cam.ac.uk>
2020-11-15 18:06:53 +0000

    Fix Style Issues

--- src/test/face_descriptors/test_HOG.py
+++ src/test/face_descriptors/test_HOG.py
@@ -5,6 +5,7 @@ from ...face_descriptors import HOG
 
 # =================== Testing Compute Gradient Function ===================
 
+
 def test_compute_gradient_grayscale():
     img = np.array([[3, 2, 1],
                     [3, 2, 1],
@@ -19,36 +20,38 @@ def test_compute_gradient_grayscale():
                               [0, 0, 0]])
 
     out = HOG.compute_gradients(img)
-    # Since magnitudes are floats here, require isclose to test if theyre similar
-    # And check that every element in the array is similar enough
-    assert (np.isclose(out[0], expectedMag).sum() == img.shape[0]*img.shape[1]) and np.array_equal(out[1], expectedAngle)
+    # Since magnitudes are floats here, require isclose to test if they're
+    # similar and check that every element in the array is similar enough
+    assert (np.isclose(out[0], expectedMag).sum() == img.shape[0] * img.shape[1]) and np.array_equal(out[1], expectedAngle)
+
 
 def test_compute_gradient_colored():
-    # Check that the gradient works as necessary for colored images too. 
+    # Check that the gradient works as necessary for colored images too.
     img = np.array([[[1, 2, 3], [2, 2, 2], [3, 2, 1]],
                     [[1, 2, 3], [2, 2, 2], [3, 2, 1]],
                     [[1, 2, 3], [2, 2, 2], [3, 2, 1]]])
-    
+
     expectedMag = np.array([[0, 2, 0],
                             [0, 2, 0],
                             [0, 2, 0]])
-    
+
     expectedAngle = np.array([[0, 0, 0],
                               [0, 0, 0],
                               [0, 0, 0]])
-    
+
     out = HOG.compute_gradients(img)
 
-    assert (np.isclose(out[0], expectedMag).sum() == img.shape[0]*img.shape[1]) and np.array_equal(out[1], expectedAngle)
+    assert (np.isclose(out[0], expectedMag).sum() == img.shape[0] * img.shape[1]) and np.array_equal(out[1], expectedAngle)
 
-# =================== Testing Block Vector Creator Function ===================
+# =================== Testing Block Vector Creator Function ==================
+
 
 def test_block_histogram_creator():
     mags = np.array([[1,  2, 4],
                      [6,  5, 8],
                      [2, 10, 3]])
 
-    angles = np.array([[ 0, 20,  75],
+    angles = np.array([[0, 20,  75],
                        [30, 40, 125],
                        [25, 35, 100]])
 
@@ -57,41 +60,48 @@ def test_block_histogram_creator():
     expectedOut = np.array([1.0, 9.0, 16.0, 1.0, 3.0, 3.0, 6.0, 2.0, 0.0])
     assert np.array_equal(out, expectedOut)
 
-def test_block_histogram_creator_on_empty_input():
-    assert np.array_equal(np.zeros(9), HOG.create_block_HOG_vector(np.array([]), np.array([])))
 
-# ====================== Testing L2 Norm Normalization Function =====================
+def test_block_histogram_creator_on_empty_input():
+    assert np.array_equal(
+        np.zeros(9), HOG.create_block_HOG_vector(np.array([]), np.array([])))
+
+# ================== Testing L2 Norm Normalization Function ==================
+
 
 def test_L2_norm_on_zero_vector():
     vec = np.zeros(9)
     assert np.array_equal(HOG.L2_norm_normalization(vec), vec)
 
+
 def test_L2_norm_on_standard_vector():
     vec = np.array([3, 4])
-    expectedNorm = np.array([3/sqrt(25 + 0.0001), 4/sqrt(25 + 0.0001    )])
+    expectedNorm = np.array([3/sqrt(25 + 0.0001), 4/sqrt(25 + 0.0001)])
     assert np.array_equal(HOG.L2_norm_normalization(vec), expectedNorm)
 
-# =================== Testing Overall HOG Feature Vector Function ===================
+# ================ Testing Overall HOG Feature Vector Function ===============
+
 
 def test_HOG_vector_on_black_image():
     image = np.zeros((64, 64, 3))
     expectedOut = np.zeros(2880)
     assert np.array_equal(expectedOut, HOG.get_HOG_feature_vector(image))
 
+
 def test_HOG_vector_on_some_inputs():
     sliceOfImg = np.array([[[1, 2, 3], [2, 2, 2]],
                            [[1, 2, 3], [2, 2, 2]]])
     img = np.zeros((64, 64, 3))
-    img[:sliceOfImg.shape[0], :sliceOfImg.shape[1], :sliceOfImg.shape[2]] = sliceOfImg
+    img[:sliceOfImg.shape[0], :sliceOfImg.shape[1],
+        :sliceOfImg.shape[2]] = sliceOfImg
     expectedOut = np.float32(np.zeros(9*(256+64)))
     expectedOut[0] = 7.0
-    expectedOut[3] = 2.98667816705 # Calculated by hand with help from the gradient function
+    # Calculated by hand with help from the gradient function
+    expectedOut[3] = 2.98667816705
     expectedOut[4] = 4 + 0.61887303294
     expectedOut[5] = 4.0
     expectedOut[9*16*16] = 7.0
-    expectedOut[9*16*16 + 3] = 2.98667816705 
+    expectedOut[9*16*16 + 3] = 2.98667816705
     expectedOut[9*16*16 + 4] = 4 + 0.61887303294
     expectedOut[9*16*16 + 5] = 4.0
     out = HOG.get_HOG_feature_vector(img)
     assert np.isclose(out, expectedOut).sum() == 2880
-    
