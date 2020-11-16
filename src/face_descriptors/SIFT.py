e49977295d4c2dbfa9788616dc871fcc018d301b
Manu Varma <mv465@cam.ac.uk>
2020-11-16 20:15:44 +0000

    Add Difference of Gaussians function and corresponding unit tests

--- src/face_descriptors/SIFT.py
+++ src/face_descriptors/SIFT.py
@@ -5,14 +5,15 @@ from math import sqrt
 # Creates a 6272-dimensional feature vector for the image
 # This is done by splitting up the image into 16x16 cells and then using a
 # sliding window of 2 cells by 2 cells to create 7x7 patches with which you
-# get a 128 dimensional vector from each patch. 
+# get a 128 dimensional vector from each patch.
+
 
 def get_octaves_and_blurring(img, num_octaves=4, num_blurs=5, sigma=sqrt(2)):
     """
     Given a grayscaled image and the number of octaves and how many images
     should be in each octave, returns a dictionary of the octaves. The value
     with key, i, element of the list corresponds to the i'th octave and the
-    j'th element in the octave is the original image blurred j times. 
+    j'th element in the octave is the original image blurred j times.
 
     Keyword Arguments:
     - img (np.array) : A grayscaled image that the octaves need to be created
@@ -28,11 +29,30 @@ def get_octaves_and_blurring(img, num_octaves=4, num_blurs=5, sigma=sqrt(2)):
     size = img.shape
     for i in range(num_octaves):
         octave = np.zeros((num_blurs, size[0], size[1]))
-        octave[0] = cv2.resize(img, size, interpolation = cv2.INTER_AREA)
+        octave[0] = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
         for j in range(1, num_blurs):
             octave[j] = cv2.GaussianBlur(octave[j - 1], (0, 0),
-                            sigmaX=sigma**j, sigmaY=sigma**j)
+                                         sigmaX=sigma**j, sigmaY=sigma**j)
         size = (int(size[0]/2), int(size[1]/2))
         octaves[i + 1] = octave
 
     return octaves
+
+
+def difference_of_gaussians(octave):
+    """
+    Given an octave which is a list of the images ordered from no blur to most
+    blur, returns the difference in the blurred images.
+
+    Keyword Arguments:
+    - octave (np.array) : The octave which has the images in order from no blur
+                          to most blurred
+
+    Returns:
+    - A numpy array which is the Difference of Gaussians which approximates
+      the Laplacian of Gaussian.
+    """
+    DoG = np.zeros((octave.shape[0] - 1, octave.shape[1], octave.shape[2]))
+    for i in range(1, octave.shape[0]):
+        DoG[i - 1] = octave[i] - octave[i - 1]
+    return DoG
--- src/test/face_descriptors/test_SIFT.py
+++ src/test/face_descriptors/test_SIFT.py
@@ -4,6 +4,7 @@ from ...face_descriptors import SIFT
 
 # ========================= Testing Octaves and Blurs ========================
 
+
 def test_getting_octaves_with_64_by_64_black_image():
     img = np.zeros((64, 64))
     expectedOctaves = {}
@@ -11,15 +12,55 @@ def test_getting_octaves_with_64_by_64_black_image():
     expectedOctaves[2] = np.zeros((5, 32, 32))
     expectedOctaves[3] = np.zeros((5, 16, 16))
     expectedOctaves[4] = np.zeros((5,  8,  8))
-    np.testing.assert_equal(SIFT.get_octaves_and_blurring(img), expectedOctaves)
+    np.testing.assert_equal(
+        SIFT.get_octaves_and_blurring(img), expectedOctaves)
+
 
 def test_getting_octaves_with_4_by_4_image_and_2_octaves_and_2_per_octave_with_high_blurring():
     img = np.float32(np.array([[2, 2, 2, 2],
                                [2, 2, 2, 2],
                                [3, 3, 3, 3],
                                [3, 3, 3, 3]]))
-    out = SIFT.get_octaves_and_blurring(img, num_octaves=2, num_blurs=2, sigma=10)
+    out = SIFT.get_octaves_and_blurring(
+        img, num_octaves=2, num_blurs=2, sigma=10)
     expectedOctaves = {}
     expectedOctaves[1] = np.array([img, np.ones((4, 4))*2.5])
     expectedOctaves[2] = np.array([[[2, 2], [3, 3]], np.ones((2, 2))*2.5])
     assert all(np.allclose(out[key], expectedOctaves[key]) for key in out)
+
+# ===================== Testing Difference of Gaussians ======================
+
+
+def test_difference_of_gaussians_on_3_images():
+    octave = np.array([[[2, 2, 2, 2],
+                        [2, 2, 2, 2],
+                        [3, 3, 3, 3],
+                        [3, 3, 3, 3]],
+
+                       [[2.2, 2.2, 2.2, 2.2],
+                        [2.2, 2.2, 2.2, 2.2],
+                        [2.8, 2.8, 2.8, 2.8],
+                        [2.8, 2.8, 2.8, 2.8]],
+
+                       np.ones((4, 4)) * 2.5])
+
+    expectedDoG = np.array([[[0.2,  0.2,  0.2,  0.2],
+                             [0.2,  0.2,  0.2,  0.2],
+                             [-0.2, -0.2, -0.2, -0.2],
+                             [-0.2, -0.2, -0.2, -0.2]],
+
+                            [[0.3,  0.3,  0.3,  0.3],
+                             [0.3,  0.3,  0.3,  0.3],
+                             [-0.3, -0.3, -0.3, -0.3],
+                             [-0.3, -0.3, -0.3, -0.3]]])
+
+    DoG = SIFT.difference_of_gaussians(octave)
+    # Need to use isclose due to floating point
+    assert np.isclose(DoG, expectedDoG).sum() == (
+        expectedDoG.shape[0]*expectedDoG.shape[1]*expectedDoG.shape[2])
+
+
+def test_difference_of_gaussians_on_3_black_images():
+    octave = np.zeros((3, 64, 64))
+    expectedOctave = np.zeros((2, 64, 64))
+    assert np.array_equal(SIFT.difference_of_gaussians(octave), expectedOctave)
