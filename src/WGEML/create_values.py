412557743f7331d1d5b04d5d6e744419c8e677df
Manu Varma <mv465@cam.ac.uk>
2020-12-22 20:18:25 +0000

    Add documentation and structure to the main function

---
+++ src/WGEML/create_values.py
@@ -0,0 +1,34 @@
+from . import *
+import numpy as np
+
+
+def get_all_values_for_a_relationship(posPairSet, negPairSet):
+    """
+    Given the positive pair set and negative pair set returns a tuple
+    (U, w) where U is an array of the transformation matrices for each view
+    and w is an array of the combination weights for each view, where a view
+    is a feature descriptor of the face.
+
+    Keyword Arguments:
+    - posPairSet: A M-tuple where M is the number of feature descriptors that
+    are used. Each element of the tuple is a 2-tuple of numpy arrays of shape
+    (N, size) where N is the number of samples used to train and size is the
+    size of that specific type of descriptor. If this tuple is (x, y), then
+    for all i = 1, ..., N, we have that x[i] and y[i] form a positive pair
+    for this relationship.
+
+    - negPairSet: The same as posPairSet except x[i] and y[i] don't have the
+    specified kinship relationship.
+
+    Returns:
+    - U: The transformation matrix for the relationship
+    - w: The combination weights for the relationship
+    """
+    
+    # For each view, p:
+    #   Search the K-nearest neighbors of x_i^p and y_i^p with Euclidean distance for i = 1, ..., N
+    #   Construct the matrices S_p, D_p, D_{1p}, D_{2p} using the nearest neighbors
+    #   Modify S_p in a way so it isn't near singular
+    #   Get U_p using all the matrices obtained
+    # Obtain w using U = [U_1, ..., U_M] for all views
+
