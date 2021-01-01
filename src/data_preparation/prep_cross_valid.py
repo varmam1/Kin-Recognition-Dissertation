ff5bf247750dfc0231364afdc529a6c68e0da635
Manu Varma <mv465@cam.ac.uk>
2021-01-01 22:33:13 +0000

    Initialize DataPrep Files

---
+++ src/data_preparation/prep_cross_valid.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.io import loadmat
+
+
+# This file is to prepare the splits and positive and negative data pairs
+NUMBER_OF_FOLDS = 5
+
+
+# ====================== KinFaceW preparation functions ======================
+
---
+++ src/test/data_preparation/test_prep_cross_valid.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pytest
+from ...data_preparation import prep_cross_valid
+
