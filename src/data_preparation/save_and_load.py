1bf36d9b90aa84efa4a3b30760635f21b6a9d30f
Manu Varma <mv465@cam.ac.uk>
2021-01-06 01:16:44 +0000

    Add function to save the results of the WGEML algorithm

--- src/data_preparation/save_and_load.py
+++ src/data_preparation/save_and_load.py
@@ -24,7 +24,19 @@ def unpickle_face_descriptors(dataset):
     return fd_maps
 
 
-def save_w_and_U(ws, trans_matrices, relationship):
+def save_w_and_U(ws, trans_matrices, relationship, dataset):
     """
+    For a relationship, ex. father-daughter, and the dataset, save all of the
+    w and U vectors and matrices for each training set in a map and saves on
+    disk.
 
+    Keyword Arguments:
+    - ws: A list of the w vector for each training set of the cross validation
+    - trans_matrices: A list of the U matrix for each training set
+    - relationship: What relationship is being tested (ex. father-daughter, etc.)
+    - dataset: The name of the dataset that this is being done with. Ex. KinFaceW-I
     """
+    pathToSave = dataPath + dataset + "/WGEML_out/"
+    WGEML_results = open(pathToSave + "/" + relationship + "_out.pkl", "wb")
+    pickle.dump({"w": ws, "U" : trans_matrices}, WGEML_results)
+    WGEML_results.close()
